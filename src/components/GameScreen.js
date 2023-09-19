73379525a1657b73871e8a92c324f90a943ecb6d
Kristóf Bence <bencekristof06@gmail.com>
2023-09-19 19:06:41 +0200

    I hope God forgives me for this

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,41 +1,74 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import style from "./GameScreen.module.css";
 import { auth, db } from "../config/firebase";
 import { getDocs, collection, query, where, orderBy } from "firebase/firestore";
 
+const GameScreen = (props) => {
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
 
-const GameScreen = props => {
+  const colRef = collection(db, "questions");
+  useEffect(() => {
+    //READ THE DATA
+    const getQuestions = async () => {
+      try {
+        const data = await getDocs(colRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
 
-    const [allQuestions, setAllQuestions] = useState([])
-    const [questions, setQuestions] = useState([])
+        const getRandomQuestions = () => {
+          const randomQuestions = new Set();
+          while (randomQuestions.size < props.gameLength) {
+            let x = Math.floor(Math.random() * filteredData.length);
+            randomQuestions.add(filteredData[x]);
+          }
+          setQuestions(Array.from(randomQuestions))
+        };
+        getRandomQuestions();
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-    const colRef = collection(db, "questions");
-    useEffect(() => {
-        //READ THE DATA
-        const getQuestions = async () => {
-            try{
-                const data = await getDocs(colRef);
-                const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}));
-                setAllQuestions(filteredData)
-            } catch (err) {
-                console.error(err)
-            }
-        }
-        getQuestions()
+    getQuestions();
+  }, []);
 
-    }, [allQuestions, questions]);
+  const nextQuestion = () => {
+    
+  }
+
+  const checkAnswer = (ans) => {
+    if(ans === questions[index].correctAns){
+        setScore((prevScore) => {return prevScore+1})
+    }
+    increaseAndCheckIndex()
+  }
+
+  const increaseAndCheckIndex = () => {
+    setIndex((prevIndex) => {return prevIndex + 1});
+    if(index >= props.gameLength){
+        
+    }
+  }
+  
 
 
-    return (
-        <div className={style.gameDiv}>
-            <h2>Question</h2>
-            {props.gameLength}
-            {allQuestions.map(q => {
-                <p>{q.questions}</p>
-            })}
-        </div>
-    )
-}
+  return (
+    <div className={style.gameDiv}>
+      {questions[index] && <h2>{questions[index].question}</h2>}
+      {props.gameLength}
+      <div className={style.answers}>
+        {questions[index] && <button onClick={() => {checkAnswer(questions[index].ans1)}}>{questions[index].ans1}</button>}
+        {questions[index] && <button onClick={() => {checkAnswer(questions[index].ans2)}}>{questions[index].ans2}</button>}
+        {questions[index] && <button onClick={() => {checkAnswer(questions[index].ans3)}}>{questions[index].ans3}</button>}
+        {questions[index] && <button onClick={() => {checkAnswer(questions[index].ans4)}}>{questions[index].ans4}</button>}
+      </div>
+      {score}
+    </div>
+  );
+};
 
 export default GameScreen;
-
